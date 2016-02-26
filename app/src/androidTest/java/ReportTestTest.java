589ff853ccd31a1addb6316c32dddb92aa3cc5d7
cam <caeparra@gmail.com>
2016-02-25 20:38:24 -0700

    fixes to test

---
+++ app/src/androidTest/java/ReportTestTest.java
@@ -0,0 +1,5 @@
+/**
+ * Created by cam on 2/25/16.
+ */
+public class ReportTestTest {
+}
---
+++ app/src/main/java/com/github/workcubed/Report.java
@@ -0,0 +1,9 @@
+package com.github.workcubed;
+
+/**
+ * Created by cam on 2/25/16.
+ */
+public class Report {
+
+
+}
