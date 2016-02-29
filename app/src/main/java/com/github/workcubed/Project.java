d7bb7503e804578ee0397019ee58152714685f5f
Ben <bis11012@byui.edu>
2016-02-29 08:47:49 -0700

    Project.java  -> added connect() and base code for it.

--- app/src/main/java/com/github/workcubed/Project.java
+++ app/src/main/java/com/github/workcubed/Project.java
@@ -1,5 +1,9 @@
 package com.github.workcubed;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 /**
  * Created by cam on 2/24/16.
  */
@@ -34,6 +38,20 @@ public class Project {
 
     public void delete_task () {
 
+    }
+
+    public void connect () {
+        String url = "jdbc:mysql://localhost:3306/javabase";
+        String username = "java";
+        String password = "password";
+
+        System.out.println("Connecting database...");
+
+        try (Connection connection = DriverManager.getConnection(url, username, password)) {
+            System.out.println("Database connected!");
+        } catch (SQLException e) {
+            throw new IllegalStateException("Cannot connect the database!", e);
+        }
     }
 
 }
