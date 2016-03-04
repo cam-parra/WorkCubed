9b0797bbfa6d1ffb68827254523911715f288f41
david short <sho13014@byui.edu>
2016-03-03 19:22:20 -0700

    Wrote add and edit methods for task and project classes

--- app/src/main/java/com/github/workcubed/Project.java
+++ app/src/main/java/com/github/workcubed/Project.java
@@ -1,57 +1,86 @@
 package com.github.workcubed;
 
 import java.sql.Connection;
+import java.sql.Date;
 import java.sql.DriverManager;
+import java.sql.PreparedStatement;
 import java.sql.SQLException;
 
 /**
- * Created by cam on 2/24/16.
+ * Created by Dave on 2/24/16.
  */
 
 
 
 public class Project {
 
-    int year;
+    public void add_project (String name, String description, Date date_created, Date deadline_date, boolean status) {
 
-    public int add_project () {
+        String url = "workcubeddb.cj4vqnmu1rwe.us-west-2.rds.amazonaws.com:1150";
+        String username = "cam";
+        String password = "octocat246";
 
-        return 0;
+        try (Connection connection = DriverManager.getConnection(url, username, password)) {
+            System.out.println("Database connected!");
+
+            // the mysql insert statement
+            String query = " insert into project (name, description, date_created, deadline_date, status)"
+                    + " values (?, ?, ?, ?)";
+
+            // create the mysql insert preparedstatement
+            PreparedStatement preparedStmt = connection.prepareStatement(query);
+            preparedStmt.setString(1, name);
+            preparedStmt.setString (2, description);
+            preparedStmt.setDate(3, date_created);
+            preparedStmt.setDate(4, deadline_date);
+            preparedStmt.setBoolean(5, status);
+
+            // execute the preparedstatement
+            preparedStmt.execute();
+
+            connection.close();
+
+        } catch (SQLException e) {
+            throw new IllegalStateException("Cannot connect the database!", e);
+        }
 
     }
 
-    public void edit_project () {
+    public void edit_project (String name, String description, Date date_created, Date deadline_date, boolean status) {
+        try
+        {
+            // create a java mysql database connection
+            String myDriver = "org.gjt.mm.mysql.Driver";
+            String myUrl = "workcubeddb.cj4vqnmu1rwe.us-west-2.rds.amazonaws.com:1150";
+            String username = "cam";
+            String password = "octocat246";
+            Class.forName(myDriver);
+            Connection conn = DriverManager.getConnection(myUrl, username, password);
+
+            // create the java mysql update preparedstatement
+            String query = "update users set name = ?, description = ?, date_created = ?, deadline_date = ?, status = ? where id = ?";
+            PreparedStatement preparedStmt = conn.prepareStatement(query);
+            preparedStmt.setString(1, name);
+            preparedStmt.setString(2, description);
+            preparedStmt.setDate(3, date_created);
+            preparedStmt.setDate(4, deadline_date);
+            preparedStmt.setBoolean(5, status);
+//            preparedStmt.setInt(6, id);
+
+            // execute the java preparedstatement
+            preparedStmt.executeUpdate();
+
+            conn.close();
+        }
+        catch (Exception e)
+        {
+            System.err.println("Could not connect to database!");
+            System.err.println(e.getMessage());
+        }
 
     }
 
     public void delete_project () {
 
     }
-
-    public void add_task () {
-
-    }
-
-    public void edit_task () {
-
-    }
-
-    public void delete_task () {
-
-    }
-
-    public void connect () {
-        String url = "jdbc:mysql://localhost:3306/javabase";
-        String username = "java";
-        String password = "password";
-
-        System.out.println("Connecting database...");
-
-        try (Connection connection = DriverManager.getConnection(url, username, password)) {
-            System.out.println("Database connected!");
-        } catch (SQLException e) {
-            throw new IllegalStateException("Cannot connect the database!", e);
-        }
-    }
-
 }
--- app/src/main/java/com/github/workcubed/Task.java
+++ app/src/main/java/com/github/workcubed/Task.java
@@ -1,51 +1,84 @@
 package com.github.workcubed;
 
 import java.sql.Connection;
+import java.sql.Date;
 import java.sql.DriverManager;
+import java.sql.PreparedStatement;
 import java.sql.SQLException;
 
 /**
  * Created by cam on 2/24/16.
  */
 public class Task {
-    int start_day;
-    int start_month;
-    int start_year;
-    int end_day;
-    int end_month;
-    int end_year;
-    String description;
-    float time_to_complete_in_hours;
-    float time_spent_in_hours;
-    float reminder_time;
 
-    public static int add () {
-        return 0;
+    public static void add (String name, String description, float hours_actual, float hours_estimated, int project_id, Date deadline_date, boolean status) {
+
+        String url = "workcubeddb.cj4vqnmu1rwe.us-west-2.rds.amazonaws.com:1150";
+        String username = "cam";
+        String password = "octocat246";
+
+        try (Connection connection = DriverManager.getConnection(url, username, password)) {
+            System.out.println("Database connected!");
+
+            // the mysql insert statement
+            String query = " insert into task (description, hours_actual, hours_estimated, project_id, deadline, status)"
+                    + " values (?, ?, ?, ?)";
+
+            // create the mysql insert preparedstatement
+            PreparedStatement preparedStmt = connection.prepareStatement(query);
+            preparedStmt.setString (1, description);
+            preparedStmt.setFloat(2, hours_actual);
+            preparedStmt.setFloat(3, hours_estimated);
+            preparedStmt.setInt(4, project_id);
+            preparedStmt.setDate(5, deadline_date);
+            preparedStmt.setBoolean(6, status);
+
+            // execute the preparedstatement
+            preparedStmt.execute();
+
+            connection.close();
+
+        } catch (SQLException e) {
+            throw new IllegalStateException("Cannot connect the database!", e);
+        }
     }
 
-    public void edit () {
+    public void edit (String name, String description, float hours_actual, float hours_estimated, int project_id, Date deadline_date, boolean status) {
+        try
+        {
+            // create a java mysql database connection
+            String myDriver = "org.gjt.mm.mysql.Driver";
+            String myUrl = "workcubeddb.cj4vqnmu1rwe.us-west-2.rds.amazonaws.com:1150";
+            String username = "cam";
+            String password = "octocat246";
+            Class.forName(myDriver);
+            Connection conn = DriverManager.getConnection(myUrl, username, password);
 
+            // create the java mysql update preparedstatement
+            String query = "update users set name = ?, description = ?, hours_actual = ?, hours_estimated = ?, project_id = ?, deadline_date = ?, status = ? where id = ?";
+            PreparedStatement preparedStmt = conn.prepareStatement(query);
+            preparedStmt.setString(1, name);
+            preparedStmt.setString(2, description);
+            preparedStmt.setFloat(3, hours_actual);
+            preparedStmt.setFloat(4, hours_estimated);
+            preparedStmt.setInt(5, project_id);
+            preparedStmt.setDate(6, deadline_date);
+            preparedStmt.setBoolean(7, status);
+//            preparedStmt.setBoolean(8, id);
+
+            // execute the java preparedstatement
+            preparedStmt.executeUpdate();
+
+            conn.close();
+        }
+        catch (Exception e)
+        {
+            System.err.println("Got an exception! ");
+            System.err.println(e.getMessage());
+        }
     }
 
     public void delete () {
 
     }
-
-    public static boolean connect () {
-
-        String url = "jdbc:mysql://localhost:3306/javabase";
-        String username = "java";
-        String password = "password";
-
-        System.out.println("Connecting database...");
-
-        try (Connection connection = DriverManager.getConnection(url, username, password)) {
-            System.out.println("Database connected!");
-        } catch (SQLException e) {
-            throw new IllegalStateException("Cannot connect the database!", e);
-        }
-
-        return false;
-    }
-
 }
