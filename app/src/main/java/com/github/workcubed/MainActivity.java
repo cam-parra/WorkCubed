de87b254d1eb54ce891a85ac590e621f068455ae
Ben <bis11012@byui.edu>
2016-03-05 16:07:33 -0700

    MainActivity now has the start of a new cool listview!!!!

--- app/src/main/java/com/github/workcubed/MainActivity.java
+++ app/src/main/java/com/github/workcubed/MainActivity.java
@@ -1,5 +1,7 @@
 package com.github.workcubed;
 
+
+import android.app.ListActivity;    // would be needed if MainActivity extended ListActivity.
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -8,6 +10,15 @@ import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -15,6 +26,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
 //        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
 //        setSupportActionBar(toolbar);
 
@@ -32,6 +44,7 @@ public class MainActivity extends AppCompatActivity {
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.menu_main, menu);
+
         return true;
     }
 
@@ -49,4 +62,28 @@ public class MainActivity extends AppCompatActivity {
 
         return super.onOptionsItemSelected(item);
     }
+
+    public static ArrayList<Project> getAllCustomer() throws ClassNotFoundException, SQLException {
+
+        String myDriver = "org.gjt.mm.mysql.Driver";
+        String url = "http://workedcubed.cj4vqnmu1rwe.us-west-2.rds.amazonaws.com";
+        String username = "crew";
+        String password = "pizza123";
+
+        try (Connection connection = DriverManager.getConnection(url, username, password)) {
+            System.out.println("Database connected!");
+
+        Statement stm;
+        stm = connection.createStatement();
+        String sql = "Select * From Customer";
+        ResultSet rst;
+        rst = stm.executeQuery(sql);
+        ArrayList<Project> customerList = new ArrayList<>();
+        while (rst.next()) {
+            Project project = new Project(rst.getInt("ID"), rst.getString("Description"), rst.getTimestamp("DateCreated"),
+                    rst.getDate("DateDeadLine"), rst.getInt("Completed"),rst.getString("Name")) ;
+        }
+        return customerList;
+    }
+
 }
