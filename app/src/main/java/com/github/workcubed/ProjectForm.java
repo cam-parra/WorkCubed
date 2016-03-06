b30ad0eb729ea2fba8d1267ee0663962bf9be741
cam <caeparra@gmail.com>
2016-03-05 17:19:45 -0700

    more gui updates to the project screen

--- app/src/main/java/com/github/workcubed/ProjectForm.java
+++ app/src/main/java/com/github/workcubed/ProjectForm.java
@@ -7,88 +7,116 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.support.v4.app.FragmentManager;
+import android.widget.Button;
 import android.widget.DatePicker;
 import android.widget.TextView;
 import android.widget.Toast;
+import android.app.DatePickerDialog.OnDateSetListener;
 
 import org.w3c.dom.Text;
 
 import java.util.Calendar;
 
 public class ProjectForm extends AppCompatActivity {
+    private TextView startDateDisplay;
+    private TextView endDateDisplay;
+    private Button startPickDate;
+    private Button endPickDate;
+    private Calendar startDate;
+    private Calendar endDate;
 
-    private DatePicker datePicker;
-    private Calendar calendar;
-    private TextView dateView;
-    private TextView dateView2;
-    private  int year, month, day;
-    private  int year2, month2, day2;
+    static final int DATE_DIALOG_ID = 0;
 
-    @Override
+    private TextView activeDateDisplay;
+    private Calendar activeDate;
+
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_project_form);
 
-        dateView = (TextView) findViewById(R.id.textView);
-        dateView2 = (TextView) findViewById(R.id.textView2);
-        calendar = Calendar.getInstance();
-        year = calendar.get(Calendar.YEAR);
-        year2 = calendar.get(Calendar.YEAR);
+        /*  capture our View elements for the start date function   */
+        startDateDisplay = (TextView) findViewById(R.id.textView);
+        startPickDate = (Button) findViewById(R.id.button2);
 
+        /* get the current date */
+        startDate = Calendar.getInstance();
+
+        /* add a click listener to the button   */
+        startPickDate.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                showDateDialog(startDateDisplay, startDate);
+            }
+        });
+
+        /* capture our View elements for the end date function */
+        endDateDisplay = (TextView) findViewById(R.id.textView2);
+        endPickDate = (Button) findViewById(R.id.button3);
+
+        /* get the current date */
+        endDate = Calendar.getInstance();
+
+        /* add a click listener to the button   */
+        endPickDate.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                showDateDialog(endDateDisplay, endDate);
+            }
+        });
+
+        /* display the current date (this method is below)  */
+        updateDisplay(startDateDisplay, startDate);
+        updateDisplay(endDateDisplay, endDate);
+    }
+
+    private void updateDisplay(TextView dateDisplay, Calendar date) {
+        dateDisplay.setText(
+                new StringBuilder()
+                        // Month is 0 based so add 1
+                        .append(date.get(Calendar.MONTH) + 1).append("-")
+                        .append(date.get(Calendar.DAY_OF_MONTH)).append("-")
+                        .append(date.get(Calendar.YEAR)).append(" "));
 
     }
 
-    @SuppressWarnings("deprecation")
-    public void setDate(View view) {
-        showDialog(999);
-        Toast.makeText(getApplicationContext(), "ca", Toast.LENGTH_SHORT)
-                .show();
-    }
-    @SuppressWarnings("deprecation")
-    public void setDate2(View view) {
-        showDialog(998);
-        Toast.makeText(getApplicationContext(), "ca", Toast.LENGTH_SHORT)
-                .show();
+    public void showDateDialog(TextView dateDisplay, Calendar date) {
+        activeDateDisplay = dateDisplay;
+        activeDate = date;
+        showDialog(DATE_DIALOG_ID);
     }
 
-
-    @Override
-    protected Dialog onCreateDialog(int id) {
-        // TODO Auto-generated method stub
-        if (id == 999) {
-            return new DatePickerDialog(this, myDateListener, year, month, day);
-        }
-        if (id == 998) {
-            return new DatePickerDialog(this, myDateListener, year2, month2, day2);
-        }
-
-        return null;
-    }
-
-
-
-    private DatePickerDialog.OnDateSetListener myDateListener = new DatePickerDialog.OnDateSetListener() {
+    private OnDateSetListener dateSetListener = new OnDateSetListener() {
         @Override
-        public void onDateSet(DatePicker arg0, int arg1, int arg2, int arg3) {
-            // TODO Auto-generated method stub
-            // arg1 = year
-            // arg2 = month
-            // arg3 = day
-            showDate(arg1, arg2+1, arg3);
+        public void onDateSet(DatePicker view, int year, int monthOfYear, int dayOfMonth) {
+            activeDate.set(Calendar.YEAR, year);
+            activeDate.set(Calendar.MONTH, monthOfYear);
+            activeDate.set(Calendar.DAY_OF_MONTH, dayOfMonth);
+            updateDisplay(activeDateDisplay, activeDate);
+            unregisterDateDisplay();
         }
     };
 
-    private void showDate(int year, int month, int day) {
-        dateView.setText(new StringBuilder().append(day).append("/")
-                .append(month).append("/").append(year));
-
+    private void unregisterDateDisplay() {
+        activeDateDisplay = null;
+        activeDate = null;
     }
 
-    private void showEndDate(int year, int month, int day) {
-        dateView2.setText(new StringBuilder().append(day2).append("/")
-                .append(month2).append("/").append(year2));
-
+    @Override
+    protected Dialog onCreateDialog(int id) {
+        switch (id) {
+            case DATE_DIALOG_ID:
+                return new DatePickerDialog(this, dateSetListener, activeDate.get(Calendar.YEAR), activeDate.get(Calendar.MONTH), activeDate.get(Calendar.DAY_OF_MONTH));
+        }
+        return null;
     }
 
+    @Override
+    protected void onPrepareDialog(int id, Dialog dialog) {
+        super.onPrepareDialog(id, dialog);
+        switch (id) {
+            case DATE_DIALOG_ID:
+                ((DatePickerDialog) dialog).updateDate(activeDate.get(Calendar.YEAR), activeDate.get(Calendar.MONTH), activeDate.get(Calendar.DAY_OF_MONTH));
+                break;
+        }
+    }
 
 }
+
