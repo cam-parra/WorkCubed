6070639e66decef14f364b9be5c2c8caf40c5232
Ben <bis11012@byui.edu>
2016-03-17 18:21:05 -0600

    Created CalendarInterface interface. Check to make sure it has all it needs

---
+++ app/src/main/java/com/github/workcubed/CalendarInterface.java
@@ -0,0 +1,14 @@
+package com.github.workcubed;
+
+/**
+ * Created by Ben on 3/17/2016.
+ */
+public interface CalendarInterface {
+    void daily();
+
+    void weekly();
+
+    void monthly();
+
+    void quarterly();
+}
